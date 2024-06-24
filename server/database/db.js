423864c9df7d594ff6ee034d93f5fbea85feda5c
shubhampatil5058 <134854438+shubhampatil5058@users.noreply.github.com>
2024-06-24 20:09:37 +0530

    db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://shubhampatil3493:Shubham%409999@cluster0.vxwnpvm.mongodb.net/ECOMMERCE?retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://shubhampatil3493:Shubham%409999@cluster0.vxwnpvm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
     try {
         await mongoose.connect(URL, { 
