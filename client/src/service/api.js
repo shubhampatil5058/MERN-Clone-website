620d6aa1e531035959e8607a32f0b20692b66a3b
shubhampatil5058 <134854438+shubhampatil5058@users.noreply.github.com>
2024-06-24 20:18:05 +0530

    api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
-
+const url = 'https://mern-clone-website-git-main-shubhampatil5058s-projects.vercel.app';
+//const url = 'http://localhost:8000';
 export const authenticateLogin = async (user) => {
     try {
         return await axios.post(`${url}/login`, user);
